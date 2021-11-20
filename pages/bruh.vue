c52ae71d0a5b9f14cd307b4a902b66f4884a0b05
owen <91567654+sussy-balls-impostor@users.noreply.github.com>
2021-11-20 21:38:06 +0100

    Initial commit

---
+++ pages/bruh.vue
@@ -0,0 +1,74 @@
+<template>
+<div>
+<h1 class="red">AMONG US HATE WEBSITE</h1>
+<div class="row">
+  <div class="column">
+    <img src="puke.gif" alt="puke" style="width:50%">
+  </div>
+  <div class="column">
+    <img src="angry.gif" alt="angry" style="width:50%">
+  </div>
+  <div class="column">
+    <img src="impostor.png" alt="sussy" style="width:50%">
+  </div>
+  <div class="column">
+    <img src="suprised.png" alt="suprised" style="width:50%">
+  </div>
+</div>
+<p>like and subscribe if you</p>
+<ul>
+  <li class="purple">hate amogus</li>
+  <li class="orange">dont like sussy impostors</li>
+  <li class="green">dispise being ejected</li>
+  <li class="blue">enjoy chocolate ice cream</li>
+  <li class="red">want to destroy the world</li>
+</ul>
+</div>
+
+</template>
+<style>
+.red {
+    color: red;
+  }
+.green {
+    color: green;
+  }
+.orange {
+    color: orange;
+  }
+.blue {
+    color: blue;
+  }
+.purple {
+    color: purple;
+  }
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 25%;
+  padding: 1px;
+}
+
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+h1 {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 60px;
+}
+p {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 50px;
+}
+li {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 40px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1 class="blue">bing chilling</h1>
+    <img src="~/static/bingchilling.png" />
+    <p>
+      the chinese government is coming to kill me
+    </p>
+    <p v-if="rendering === 'server'">
+      please send help
+    </p>
+    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
+    <ul>
+      <li>oh no</li>
+      <li>i am going to die</li>
+      <li>mika is cringe</li>
+    </ul>
+
+    <NuxtLink to="/bruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmoment">this goes nowhere</NuxtLink>
+    <p> </p>
+    <NuxtLink to="/drip-page">militant drip enjoyer</NuxtLink>
+  </div>
+
+</template>
+<script>
+export default {
+  asyncData() {
+    return {
+      rendering: process.server ? 'server' : 'client'
+    }
+  }
+}
+</script>
+
+<style>
+.blue {
+    color: blue;
+  }
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+h1 {
+  font-family: 'Open Sans', sans-serif;
+}
+p {
+  font-family: 'Open Sans', sans-serif;
+}
+li {
+  font-family: 'Open Sans', sans-serif;
+}
+NuxtLink {
+  font-family: 'Open Sans', sans-serif;
+}
+
+</style>
+
