58d3a221c0d4e7edf51c9cbf3a33242a65e28c2c
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-20 22:55:47 +0100

    sda

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
+  <div class=invertedbox>
+    <h1 class=white>
+      Boh!
+    </h1>
+  </div>
   <div>
-
+    
   </div>
 
 </template>
@@ -11,18 +16,40 @@ export default {
 </script>
 
 <style>
+.red {
+    color: red;
+  }
+.green {
+    color: green;
+  }
+.orange {
+    color: orange;
+  }
 .blue {
     color: blue;
   }
+.purple {
+    color: purple;
+  }
+.white {
+  color: white;
+}
+
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 h1 {
   font-family: 'Open Sans', sans-serif;
+  font-size: 50px;
 }
 p {
   font-family: 'Open Sans', sans-serif;
+  font-size: 25px;
 }
 li {
   font-family: 'Open Sans', sans-serif;
+  font-size: 25px;
 }
-
+.invertedbox {
+  background-color: black;
+  text-align: center;
+}
 </style>
