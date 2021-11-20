b464ad7e694ed23874a89ad2cb18935bd7840b0c
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-20 22:22:02 +0100

    push (out my ass)

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global App headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'suspekt',
+    title: 'The Boh!',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'dropf dropf dropf',
+        content: 'Boh! Das hätte ich aber anders formuliert.',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
@@ -24,7 +24,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,12 @@
 <template>
   <div>
-    <h1 class="blue">bing chilling</h1>
-    <img src="~/static/bingchilling.png" />
-    <p>
-      the chinese government is coming to kill me
-    </p>
-    <p v-if="rendering === 'server'">
-      please send help
-    </p>
-    <p v-if="rendering === 'client'">Navigation is done on client side.</p>
-    <ul>
-      <li>oh no</li>
-      <li>i am going to die</li>
-      <li>mika is cringe</li>
-    </ul>
 
-    <NuxtLink to="/bruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmomentbruhmoment">this goes nowhere</NuxtLink>
-    <p> </p>
-    <NuxtLink to="/drip-page">militant drip enjoyer</NuxtLink>
   </div>
 
 </template>
 <script>
 export default {
-  asyncData() {
-    return {
-      rendering: process.server ? 'server' : 'client'
-    }
-  }
+
 }
 </script>
 
@@ -45,9 +24,5 @@ p {
 li {
   font-family: 'Open Sans', sans-serif;
 }
-NuxtLink {
-  font-family: 'Open Sans', sans-serif;
-}
 
 </style>
-
