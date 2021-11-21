fc3f972af9548f321b8958c37c599c064d42cafb
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-21 19:45:55 +0100

    404 tiddie

---
+++ pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <nuxt-link
+    tag="img"
+    :src="require('~/static/404error.gif')"
+    to="/index">
+  </nuxt-link>
+</template>
+
+<style>
+body {
+  background-color: black;
+}
+</style>
+
+<script>
+</script>
