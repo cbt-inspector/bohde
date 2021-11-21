bf0dc61ce70d21628f8a9dc7d9e5778459311d9d
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-22 00:03:33 +0100

    please work

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,18 @@
 <template>
+  <div class="ticker">
+    <div class="ticker-news">
+      <span>
+        <span>Der code funktioniert nicht...</span>
+        <span>Luca hat die Hausaufgaben vergessen...</span>
+        <span>Ich(die webseite) begehe suizid...</span>
+        <span>Das hätte ich aber anders formuliert...</span>
+        <span>Raucher rauchen (es stinkt)...</span>
+        <span>Boh!...</span>
+        <span>[Insert Bee Movie transcript here]...</span>
+        <span>https://www.bebis-schleswig-holstein.de/...</span>
+      </span>
+    </div>
+  </div>
   <div class=invertedbox>
     <h1>
       Boh!
@@ -7,9 +21,6 @@
     <p>
       Die offizielle Webseite
     </p>
-  </div>
-  <div>
-    
   </div>
 </template>
 <script>
@@ -43,4 +54,41 @@ body {
   background-color: white;
   width: 70%;
 }
+
+
+.ticker-news {
+  width: 7000px;
+  padding: 10px 0;
+  color: #7c7c7c;
+  font-size: 36px;
+  background: black;
+}
+
+.ticker {
+  overflow: hidden;
+  width: 100%;
+  padding: 5px 0 120px;
+  position: fixed;
+  bottom: -50px;
+  left: 0;
+  font-family: 'Open Sans', sans-serif;
+}
+.ticker-news > * {
+  display: inline-block;
+  animation: news 15s infinite linear;
+}
+
+@keyframes news {
+  0% {
+    transform: translateX(0);
+  }
+
+  100% {
+    transform: translateX(-4135px);
+  }
+}
+
+
+
+
 </style>
