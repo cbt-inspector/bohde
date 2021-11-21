c4a2a3047bdacd4563fd1ba01dccd730c591013f
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-21 22:09:25 +0100

    work dumbass

--- pages/404.vue
+++ pages/404.vue
@@ -4,11 +4,7 @@
     :src="require('~/static/404error.gif')"
     to="/">
   </nuxt-link> -->
-<<<<<<< HEAD
   <a href="https://theboh.de/" class="img"><img src="~/static/404error.gif" width="50%" height="50%"></a>
-=======
-  <a href="https://theboh.de/"><img src="~/static/404error.gif" width="100%" height="50%"></a>
->>>>>>> 8bf47be9ccb81aa40bb96425a00786d6947d1ef7
 </template>
 
 <style>
