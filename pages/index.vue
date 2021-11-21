4b68cdfe51190247435a37c91e9bcb67113f928a
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-22 00:24:31 +0100

    incredible

--- pages/index.vue
+++ pages/index.vue
@@ -22,34 +22,35 @@
       Die offizielle Webseite
     </p>
   </div>
-</template>
-<script>
-export default {
 
-}
-</script>
+</template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+
 h1 {
   font-family: 'Open Sans', sans-serif;
   font-size: 50px;
 }
+
 p {
   font-family: 'Open Sans', sans-serif;
   font-size: 25px;
 }
+
 .invertedbox {
   background-color: black;
   text-align: center;
   padding: 1px;
   color: white;
 }
+
 body {
   margin: 0;
   height: 700px;
   overflow-y: hidden;
 }
+
 .whiteline {
   background-color: white;
   width: 70%;
@@ -60,23 +61,20 @@ body {
 }
 
 .ticker-news {
-  width: 7000px;
-  padding: 10px 0;
+  width: 6000px;
   color: #7c7c7c;
-  font-size: 36px;
+  font-size: 12px;
   background: black;
 }
 
 .ticker {
   overflow: hidden;
   width: 100%;
-  padding: 5px 0 120px;
-  position: fixed;
-  bottom: -50px;
   left: 0;
   font-family: 'Open Sans', sans-serif;
 }
-.ticker-news > * {
+
+.ticker-news>* {
   display: inline-block;
   animation: news 15s infinite linear;
 }
@@ -87,11 +85,8 @@ body {
   }
 
   100% {
-    transform: translateX(-4135px);
+    transform: translateX(-3000px);
   }
 }
-
-
-
 
 </style>
