23840f8dd950fece999b674b694572c56bd1dda2
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-21 20:01:57 +0100

    hfgds

--- pages/404.vue
+++ pages/404.vue
@@ -2,13 +2,17 @@
   <nuxt-link
     tag="img"
     :src="require('~/static/404error.gif')"
-    to="/index">
+    to="/">
   </nuxt-link>
+  <img src:"~/static/404error.gif" href="https://theboh.de/" class=largeimg>
 </template>
 
 <style>
 body {
   background-color: black;
+}
+.largeimg {
+  width: 400%;
 }
 </style>
 
