c913a767124efd3801be7b85f417fde24c269ae9
sussy-balls-impostor <eo2dog192@gmail.com>
2021-11-28 19:15:34 +0100

    complete overhaul (its all different now motherfuckers)

---
+++ components/bohfoot.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="footeradv">
+    <div class="foottable" style="margin-top:10px">
+      <table style="margin-left:auto;margin-right:auto;">
+        <tr>
+          <th colspan="3" style="align-text:center">Contact info or smth idk</th>
+        </tr>
+        <tr>
+          <td><a href= "mailto:owen@theboh.de">owen@theboh.de</a></td>
+          <td>&nbsp;|&nbsp;</td>
+          <td><a href= "mailto:luca@theboh.de">luca@theboh.de</a></td>
+          <td>&nbsp;|&nbsp;</td>
+          <td><a href= "mailto:emil@theboh.de">emil@theboh.de</a></td>
+        </tr>
+      </table>
+      <hr>
+      <a style="color: rgba(255, 255, 255, 0.5)">&copy; 2021 Das-Boh AG. All Rights Reserved</a>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+.footeradv {
+  position: relative;
+  bottom: 0px;
+  background: black;
+  width: 100%;
+  text-align: center;
+  height: 150px;
+  overflow: hidden;
+  color: rgba(255, 255, 255, 0.5);
+}
+
+.footeradv th, td, a {
+  font-family: 'Courier Prime', sans-serif;
+  color: grey;
+}
+
+.footeradv hr {
+  border: none;
+  border-top: 3px double #333;
+  color: #333;
+  overflow: visible;
+  text-align: center;
+  height: 5px;
+  width: 30%;
+}
+
+.footeradv hr:after {
+  background: black;
+  content: '§';
+  padding: 0 4px;
+  position: relative;
+  top: -13px;
+  color: grey;
+}
+
+.foottable {
+  background: black;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,180 +1,65 @@
 <template>
-  <header>
-    <div class="wrapper">
-      <div class="sidescroll">
-        <!--
-   &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-      -->
-        <p>
-          Der code funktioniert nicht &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          <i>"Cornelius Caesar"</i>-Zitat Adrian &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Ich(die webseite) begehe suizid &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Press <a href="#"><b>F</b></a> to pray to Putin &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Das hätte ich aber anders formuliert &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Raucher rauchen (es stinkt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Boh! &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          [Insert Bee Movie transcript here] &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          <a href="https://www.bebis-schleswig-holstein.de/"><b>bebis-schleswig-holstein.de</b></a> &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Ich will die Lehrer alle umbringen und dann die Schule sprengen &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Händy spiele sind schlecht bitte kauf einfach ein PC &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          kommunistische Regierungen können nicht funkionieren (dies ist ein fakt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          DaBaby says:"Let's Go!" &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-        </p>
-        <p>
-          Der code funktioniert nicht &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          <i>"Cornelius Caesar"</i>-Zitat Adrian &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Ich(die webseite) begehe suizid &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Press <a href="#"><b>F</b></a> to pray to Putin &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Das hätte ich aber anders formuliert &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Raucher rauchen (es stinkt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Boh! &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          [Insert Bee Movie transcript here] &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          <a href="https://www.bebis-schleswig-holstein.de/"><b>bebis-schleswig-holstein.de</b></a> &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Ich will die Lehrer alle umbringen und dann die Schule sprengen &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          Händy spiele sind schlecht bitte kauf einfach ein PC &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          kommunistische Regierungen können nicht funkionieren (dies ist ein fakt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-          DaBaby says:"Let's Go!" &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
-        </p>
-      </div>
-    </div>
-
-    <div class="invertedbox">
-      <h1>
-        Boh!
-      </h1>
-      <hr class="whiteline">
-      <p class="skewer">
-        Die offizielle Webseite
-      </p>
-    </div>
-    <div class=whitebckg>
-      <div class="dropdown">
-        <button class="menu">Menu</button>
-        <div class="dropdown-content">
-          <a href="/">Homepage</a>
-          <a href="/pentest">Pentesting Service</a>
-          <a href="http://www.ismycomputeron.com/">Suprise</a>
-          <a href="https://www.bibun.ga/">Bing Chilling&copy;</a>
-          <a href="https://www.bibun.ga/tacdrip.png">😳dro🅱pf😳</a>
-        </div>
-      </div>
-      <div class="row">
-        <div class="lcolumn">
-          <div class="leftcolumn">
-            <p>
-              i could put system updates here
-              <br>&nbsp;<br>
-              or maybe some <i>bing chilling</i><br>
-              <img src="~/static/kickl.jpg" alt="kickl" style="margin-left:20%;width:620px;height:2000px;"><br>
-              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; (or just <i>[<b>HERBERT KICKL</b>]</i>)
+  <div>
+    <bohhead />
+    <div class="maincontainer">
+      <bohmenu />
+<!--Text Body STARTS Here!!!!!-->
+      <div class="textbody">
+        <div class="row">
+          <div class="lcolumn">
+            <div class="leftcolumn">
+              <p>
+                one big massive shitpost
+                <br>&nbsp;<br>
+                would you like some <i>bing chilling</i><br>
+                <img src="~/static/kickl.jpg" alt="kickl" style="margin-left:20%;width:620px;height:2000px;"><br>
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; (this is senpai <i><b>HERBERT KICKL</b></i>)
 
 
 
-            </p>
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="rcolumn">
-          <div class="rightcolumn">
-            <p style="font-size:22px;margin-left:4%;margin-right:4%">
-              <b>But why Boh!??? (wieso denn boh?!?!(ich muss es wissen)??!)?!</b>
-              <br>If you’re anything like me, there’ll be plenty of times in Italy when you just don’t know the answer.
-              Why hasn’t a single bus come in 45 minutes? I don’t know. Do you really need that piece of paperwork or can you get by with a wink and a smile? I don’t know. Which region makes the best pasta? Heaven help me, I don’t know.
-              That’s why I’m such a big fan of today’s word: <b>boh</b>.
-              It means ‘I don’t know’, but in its most informal form – like when we shorten the phrase to ‘dunno’.
-              Don’t be fooled by the transcription: it doesn’t rhyme with “oh” or “so” in English. Instead the vowel is shorter: more of a “buh”.
-              And according to the dictionary, Italians have been making this funny 🅱 sound since circa 1840 to express “doubt, indifference or reticence”.<br>&nbsp;
-              <br>Der text hier hat eigentlich nichts mit Boh! zu tun und wurde einfach aus dem internet kopiert (das ist ein richtiger 🅱oh moment)<br>&nbsp;
-            </p>
+          <div class="rcolumn">
+            <div class="rightcolumn">
+              <p style="font-size:22px;margin-left:4%;margin-right:4%">
+                <b>But why Boh!??? (wieso denn boh?!?!(ich muss es wissen)??!)?!</b>
+                <br>If you’re anything like me, there’ll be plenty of times in Italy when you just don’t know the answer.
+                Why hasn’t a single bus come in 45 minutes? I don’t know. Do you really need that piece of paperwork or can you get by with a wink and a smile? I don’t know. Which region makes the best pasta? Heaven help me, I don’t know.
+                That’s why I’m such a big fan of today’s word: <b>boh</b>.
+                It means ‘I don’t know’, but in its most informal form – like when we shorten the phrase to ‘dunno’.
+                Don’t be fooled by the transcription: it doesn’t rhyme with “oh” or “so” in English. Instead the vowel is shorter: more of a “buh”.
+                And according to the dictionary, Italians have been making this funny 🅱 sound since circa 1840 to express “doubt, indifference or reticence”.<br>&nbsp;
+                <br>Der text hier hat eigentlich nichts mit Boh! zu tun und wurde einfach aus dem internet kopiert (das ist ein richtiger 🅱oh moment)<br>&nbsp;
+              </p>
+            </div>
           </div>
         </div>
       </div>
+<!--Text Body STOPS Here!!!!!-->
     </div>
-    <div class="footeradv">
-      <div class="foottable" style="margin-top:10px">
-        <table style="margin-left:auto;margin-right:auto;">
-          <tr>
-            <th colspan="3" style="align-text:center">Contact info or smth idk</th>
-          </tr>
-          <tr>
-            <td><a href= "mailto:owen@theboh.de">owen@theboh.de</a></td>
-            <td>&nbsp;|&nbsp;</td>
-            <td><a href= "mailto:luca@theboh.de">luca@theboh.de</a></td>
-            <td>&nbsp;|&nbsp;</td>
-            <td><a href= "mailto:emil@theboh.de">emil@theboh.de</a></td>
-          </tr>
-        </table>
-        <hr>
-        <a style="color: rgba(255, 255, 255, 0.5)">&copy; 2021 Das-Boh AG. All Rights Reserved</a>
-      </div>
-    </div>
-  </header>
+    <bohfoot />
+  </div>
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
-/*monospace*/
-body {
-  margin: 0;
-  overflow: auto;
-}
-
-h1 {
-  font-family: 'Open Sans', sans-serif;
-  font-size: 50px;
-}
-
-p {
-  font-family: 'Open Sans', sans-serif;
-  font-size: 25px;
-}
-
-a {
-  font-family: 'Open Sans', sans-serif;
-}
-
-.invertedbox {
-  font-family: 'Comfortaa', serif;
-  background-color: black;
-  text-align: center;
-  padding: 0;
-  color: white;
-  overflow: hidden;
-  height: 270px;
-}
-
-.invertedbox a {
-  font-family: 'Open Sans', sans-serif;
-  text-decoration: none;
-  color: white;
-}
-
-.whiteline {
-  background-color: white;
-  width: 70%;
-}
-
-.whitebckg {
+.maincontainer {
   background: white;
+  padding-bottom: 50px;
+}
+.textbody {
   height: auto;
+  margin-top: 20px;
 }
-
-.textbdy {
-  background: white;
-  height: 1000px;
-}
-
-.textbdy h2 {
+.textbody h2 {
   text-align: center;
   font-family: 'Open Sans', sans-serif;
   font-size: 28px;
 }
-
-.textbdy p {
+.textbody p {
   font-family: 'Open Sans', sans-serif;
   font-size: 18px;
   width: 70%;
@@ -183,12 +68,15 @@ a {
 .rightcolumn {
   background: rgba(230,230,230,1);
   margin-left: 15%;
-  margin-right: 15%
+  margin-right: 15%;
+  padding-top: 1px;
 }
 .leftcolumn {
   background: rgba(230,230,230,1);
   width: 90%;
   margin-left: 10%;
+  padding-top: 10px;
+  padding-bottom: 10px;
 }
 .row::after {
   content: "";
@@ -204,135 +92,5 @@ a {
   float: left;
   display: inline;
   width: 70%;
-}
-.skewer {
-  transform: skewY(-3deg);
-}
-
-.menu {
-  background-color: white;
-  border: 1px solid black;
-  color: black;
-  padding: 3px 54px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 18px;
-  transition-duration: 0.2s;
-  cursor: pointer;
-  font-family: 'Open Sans', sans-serif;
-}
-
-.menu:hover {
-  background: black;
-  color: white;
-}
-
-.dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.dropdown-content {
-  display: none;
-  position: absolute;
-  background-color: white;
-  text-align: center;
-  min-width: 100%;
-  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-  z-index: 1;
-}
-
-.dropdown-content a {
-  color: black;
-  background: white;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  transition-duration: 0.2s;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 18px
-}
-
-.dropdown-content a:hover {
-  background: black;
-  color: white;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-  background: black;
-}
-
-.dropdown:hover .dropbtn {
-  background: black;
-}
-
-.footeradv {
-  position: relative;
-  bottom: 0px;
-  background: black;
-  width: 100%;
-  text-align: center;
-  height: 150px;
-  overflow: hidden;
-  color: rgba(255, 255, 255, 0.5);
-}
-
-.footeradv th, td, a {
-  font-family: 'Courier Prime', sans-serif;
-  color: grey;
-}
-
-.footeradv hr {
-  border: none;
-  border-top: 3px double #333;
-  color: #333;
-  overflow: visible;
-  text-align: center;
-  height: 5px;
-  width: 30%;
-}
-
-.footeradv hr:after {
-  background: black;
-  content: '§';
-  padding: 0 4px;
-  position: relative;
-  top: -13px;
-  color: grey;
-}
-
-.foottable {
-  background: black;
-}
-
-.wrapper {
-  max-width: 100%;
-  overflow: hidden;
-  color: white;
-  background: black;
-}
-
-.sidescroll {
-  white-space: nowrap;
-  overflow: hidden;
-  display: inline-block;
-  animation: sidescroll 16s linear infinite;
-}
-
-.sidescroll p {
-  display: inline-block;
-  font-size: 12px;
-}
-
-@keyframes sidescroll {
-  0% {
-    transform: translate3d(0, 0, 0);
-  }
-
-  100% {
-    transform: translate3d(-50%, 0, 0);
-  }
 }
 </style>
