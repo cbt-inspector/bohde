8040d6af260ba79ea76073d606f7cde0e726f784
schlingelTV <83114842+schlingelTV@users.noreply.github.com>
2021-11-29 19:36:45 +0100

    ads

---
+++ pages/ads.vue
@@ -0,0 +1,174 @@
+<head>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4242474983410878"
+   crossorigin="anonymous"></script>
+  <div class="temp">
+    <div class="wrapper">
+      <div class="sidescroll">
+      <!--
+ &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+    -->
+        <p>
+        Der code funktioniert nicht &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        <i>"Cornelius Caesar"</i>-Zitat Adrian &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Ich(die webseite) begehe suizid &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Press <a href="#"><b>F</b></a> to pray to Putin &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Das hätte ich aber anders formuliert &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Raucher rauchen (es stinkt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Boh! &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        [Insert Bee Movie transcript here] &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        <a href="https://www.bebis-schleswig-holstein.de/"><b>bebis-schleswig-holstein.de</b></a> &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Ich will die Lehrer alle umbringen und dann die Schule sprengen &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Händy spiele sind schlecht bitte kauf einfach ein PC &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        kommunistische Regierungen können nicht funkionieren (dies ist ein fakt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        DaBaby says:"Let's Go!" &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Like and subscribe to bomb the fuck out of France &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        </p>
+        <p>
+        Der code funktioniert nicht &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        <i>"Cornelius Caesar"</i>-Zitat Adrian &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Ich(die webseite) begehe suizid &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Press <a href="#"><b>F</b></a> to pray to Putin &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Das hätte ich aber anders formuliert &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Raucher rauchen (es stinkt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Boh! &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        [Insert Bee Movie transcript here] &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        <a href="https://www.bebis-schleswig-holstein.de/"><b>bebis-schleswig-holstein.de</b></a> &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Ich will die Lehrer alle umbringen und dann die Schule sprengen &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Händy spiele sind schlecht bitte kauf einfach ein PC &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        kommunistische Regierungen können nicht funkionieren (dies ist ein fakt) &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        DaBaby says:"Let's Go!" &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        Like and subscribe to bomb the fuck out of France &nbsp; &nbsp;&mdash;&nbsp; &nbsp;
+        </p>
+      </div>
+    </div>
+
+    <div class="invertedbox">
+      <h1>
+        <a href="/" style="font-family:'Open Sans',sans-serif;text-decoration:none;color:white;">Boh!</a>
+      </h1>
+      <hr style="background-color:white;width:70%;">
+      <p class="skewer" style="transform:skewY(-3deg)">
+        Die offizielle Webseite
+      </p>
+    </div>
+  </div>
+</head>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+/*monospace*/
+body {
+margin: 0;
+}
+
+.temp {
+background: black;
+}
+
+.temp h1 {
+font-family: 'Open Sans', sans-serif;
+font-size: 50px;
+color: white;
+}
+
+.temp p {
+font-family: 'Open Sans', sans-serif;
+font-size: 25px;
+color: white;
+}
+
+.temp a {
+font-family: 'Open Sans', sans-serif;
+color: cyan;
+}
+
+.invertedbox {
+font-family: 'Comfortaa', serif;
+background-color: black;
+text-align: center;
+padding: 0;
+color: white;
+overflow: hidden;
+height: 270px;
+}
+
+.wrapper {
+max-width: 100%;
+overflow: hidden;
+color: white;
+}
+
+.sidescroll {
+white-space: nowrap;
+overflow: hidden;
+display: inline-block;
+animation: sidescroll 16s linear infinite;
+}
+
+.sidescroll p {
+display: inline-block;
+font-size: 12px;
+}
+
+@keyframes sidescroll {
+0% {
+  transform: translate3d(0, 0, 0);
+}
+
+100% {
+  transform: translate3d(-50%, 0, 0);
+}
+}
+</style>
+
+
+<template>
+  <div>
+
+    <div class="maincontainer">
+      <bohmenu />
+<!--Text Body STARTS Here!!!!!-->
+      <div class="textbody">
+        <h2>Lorem ipsum</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+<!--Text Body STOPS Here!!!!!-->
+    </div>
+    <bohfoot />
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+.maincontainer {
+  background: white;
+  padding-bottom: 50px;
+}
+.textbody {
+  background: rgba(230,230,230,1);
+  height: auto;
+  padding-top: 20px;
+  padding-bottom: 30px;
+  margin-top: 50px;
+  margin-left: 6%;
+  margin-right: 6%;
+}
+.textbody h2 {
+  text-align: center;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 28px;
+}
+.textbody p {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 18px;
+  width: 70%;
+  margin-left: 15%;
+}
+</style>
